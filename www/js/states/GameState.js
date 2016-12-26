166ffbbf7d5dfff23dde34656984f67bc371a8ae
coconutpi <rsongkuntham@wesleyan.edu>
2016-12-26 21:43:30 +0700

    player can move up and down, health decreases and hurt sprite appears briefly when touching monks

--- www/js/states/GameState.js
+++ www/js/states/GameState.js
@@ -6,8 +6,8 @@ Run.GameState = {
     init: function() {
         this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
         this.game.physics.startSystem(Phaser.Physics.ARCADE);
-        this.PLAYER_SPEED = 200;
-        this.GRASS_SPEED = 200;
+        this.PLAYER_SPEED = 350;
+        this.GRASS_SPEED = 170;
         this.MONK_SPEED = 200;
     },
     
@@ -16,6 +16,7 @@ Run.GameState = {
         this.load.image('grass', 'assets/images/grass.png');
         this.load.spritesheet('player', 'assets/images/girl.png', 32, 32, 84);
         this.load.spritesheet('monk', 'assets/images/monk.png', 32, 32);
+        this.load.image('hurt', 'assets/images/hurt.png');
     },
     
     create: function() {
@@ -32,12 +33,14 @@ Run.GameState = {
                                       this.game.world.height - 50,
                                       'player',
                                       43);
+        
         this.player.anchor.setTo(0.5);
         this.player.scale.x = 3;
         this.player.scale.y = 3;
         this.game.physics.arcade.enable(this.player);
         this.player.body.collideWorldBounds = true;
         this.animate(this.player, 42, 43, 44);
+        this.player.health = 10;
         
         //monk
         this.initMonks();
@@ -47,13 +50,29 @@ Run.GameState = {
     
     update: function() {
         this.player.body.velocity.x = 0;
+        this.player.body.velocity.y = 0;
         
         if(this.game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
+            console.log('LEFT');
             this.player.body.velocity.x -= this.PLAYER_SPEED;
         }
         else if(this.game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
+            console.log('RIGHT');
             this.player.body.velocity.x += this.PLAYER_SPEED;
         }
+        else if(this.game.input.keyboard.isDown(Phaser.Keyboard.UP)) {
+            console.log('UP');
+            this.player.body.velocity.y -= this.PLAYER_SPEED;
+        }
+        else if(this.game.input.keyboard.isDown(Phaser.Keyboard.DOWN)) {
+            console.log('DOWN');
+            this.player.body.velocity.y += this.PLAYER_SPEED;
+        }
+        
+        
+        //collision
+        this.game.physics.arcade.overlap(this.player, this.monks, this.damagePlayer, null, this);
+        
     },
     
     animate: function(sprite,walk1,walk2,walk3) {
@@ -70,13 +89,28 @@ Run.GameState = {
         var monk = this.monks.getFirstExists(false);
         
         if(!monk) {
-            monk = new Run.Monk(this.game, this.game.world.centerX, 0);
+            monk = new Run.Monk(this.game, this.game.rnd.between(0,this.game.world.width), 0);
             this.monks.add(monk);
         }
         else {
-            monk.reset(this.game.world.centerX, 0);
+            monk.reset(this.game.rnd.between(0,this.game.world.width), 0);
         }
         
         monk.body.velocity.y = this.MONK_SPEED;
+    },
+    
+    damagePlayer: function(player, monks) {
+        console.log('damage!');
+        console.log(player.health);
+        player.damage(0.005);
+        this.ouch(player);
+    },
+    
+    ouch: function(player) {
+        console.log('ouch!');
+        this.hurt = this.add.sprite(player.x, player.top, 'hurt');
+        this.hurt.scale.x = 3;
+        this.hurt.scale.y = 3;
+        this.hurt.lifespan = 1.5;
     }
 };
